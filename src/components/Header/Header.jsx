abd713823840d22628f09b5c9e113a8f9b0a5a38
Jimmy Diep <jdiep79@gmail.com>
2018-02-03 14:17:53 -0800

    add header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,19 +4,28 @@ import Link from 'gatsby-link'
 import styles from './Header.module.css';
 
 const Header = () => (
-  <div className={styles.container}>
+  <div>
+    <div className={styles.navigationContainer}>
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src="https://s3.amazonaws.com/divvy-images/Divvy_Logo_White.png" alt="logo"/>
+      </div>
       <div className={styles.linkContainer}>
-        <div>
+        <div className={styles.link}>
           <Link to="/" style={{ color: '#00779B', textDecoration: 'none'}}>Dashboard</Link>
         </div>
-        <div>
+        <div className={styles.link}>
           <Link to="/" style={{ color: '#00779B', textDecoration: 'none'}}>Projects</Link>
         </div>
-        <div>
+        <div className={styles.link}>
           <Link to="/" style={{ color: '#00779B', textDecoration: 'none'}}>Directory</Link>
         </div>
+      </div>
     </div>
+    <div className={styles.userSearchContainer}>
+      <img className={styles.find} src="https://s3.amazonaws.com/divvy-images/find.png" alt="Search" />
+      <img className={styles.user} src="https://s3.amazonaws.com/divvy-images/user.png" alt="User" />
+    </div>     
   </div>
-)
+);
 
 export default Header
